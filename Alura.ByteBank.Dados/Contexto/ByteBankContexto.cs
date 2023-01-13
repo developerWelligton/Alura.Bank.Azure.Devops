76dade6b49554f6252822b13f2a4463683c2606c
welligton_fadami <welligtoncos@gmail.com>
2023-01-12 22:19:30 -0300

    Teste CI

--- Alura.ByteBank.Dados/Contexto/ByteBankContexto.cs
+++ Alura.ByteBank.Dados/Contexto/ByteBankContexto.cs
@@ -13,8 +13,7 @@ namespace Alura.ByteBank.Dados.Contexto
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            string stringconexao = "server=localhost;" +
-                                   "DataBase=bytebankbd_webapp;Uid=root;Pwd=root";
+            string stringconexao = "Server=bytebankwebappazure.mysql.database.azure.com; Port=3306; Database=bytebankwebappazure; Uid=welligtoncos@bytebankwebappazure; Pwd=@Well32213115; SslMode=Preferred";
             optionsBuilder.UseMySql(stringconexao, 
                                     ServerVersion.AutoDetect(stringconexao));
         }
